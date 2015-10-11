d0f2774420b11f761d0a5944c276a31875e08093
GPointExtreme <dominik.geiger@hotmail.com>
2015-10-11 12:49:20 +0200

    Rechnungen hinzugefügt.

--- src/HelloGitHub.java
+++ src/HelloGitHub.java
@@ -4,6 +4,15 @@ public class HelloGitHub {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello GitHub!");
+		
+		boolean treeFront = true;
+		int a=9;
+		int b=5;
+
+		System.out.println(a + " + " + b + " = "+(a+b));
+		System.out.println(a + " - " + b + " = "+(a-b));
+		System.out.println(a + " * " + b + " = "+(a*b));
+		System.out.println(a + " / " + b + " = "+(a/b));
 
 	}
 
